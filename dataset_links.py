58fce6e6d18d99bec85454564d796939e6184aae
Sahil <sdhawan173@pm.me>
2024-03-28 14:02:44 -0400

    current working version of loading in comment code.

---
+++ dataset_links.py
@@ -0,0 +1,18 @@
+askreddit = [
+    'askreddit',
+    'https://old.reddit.com/r/AskReddit/comments/8cibi0/serious_should_elderly_people_be_forced_to_take/',
+    'https://old.reddit.com/r/AskReddit/comments/dr4tjk/serious_therapists_of_reddit_what_are_some_red/',
+    'https://old.reddit.com/r/AskReddit/comments/73e8s1/serious_people_who_check_university_applications/'
+]
+askhistorians = [
+    'askhistorians',
+    'https://old.reddit.com/r/AskHistorians/comments/id2cfp/dolly_parton_had_a_famous_song_9_to_5_yet_every/',
+    'https://old.reddit.com/r/AskHistorians/comments/km8hhn/is_it_possible_with_ancient_cultures_that_we_are/',
+    'https://old.reddit.com/r/AskHistorians/comments/7k7wun/why_are_elementaryaged_students_in_the_us/'
+]
+science = [
+    'science',
+    'https://old.reddit.com/r/science/comments/k1ofcu/in_the_us_states_typically_pay_for_prison_while/',
+    'https://old.reddit.com/r/science/comments/b6hy7e/woman_with_mutant_gene_who_feels_no_pain_and/',
+    'https://old.reddit.com/r/science/comments/w8l4b8/one_in_five_adults_dont_want_children_and_theyre/'
+]
--- forum_summarize.py
+++ forum_summarize.py
@@ -1,5 +1,9 @@
 import os
 import praw
+import pprint
+from dataset_links import askreddit
+from dataset_links import askhistorians
+from dataset_links import science
 
 
 def initialize_praw():
@@ -19,6 +23,76 @@ def initialize_praw():
 
 
 REDDIT = initialize_praw()
+AUTOMOD = 'AutoModerator'
 
 
-print(REDDIT.user.me())
+def get_moderators(subreddit_name):
+    return REDDIT.subreddit(subreddit_name).moderator()
+
+
+def load_reddit_post(url_string):
+    return REDDIT.submission(url=url_string)
+
+
+def create_comment_list(post):
+    sorted_ids = sorted(post.comments, key=lambda comment: comment.score, reverse=True)
+    reddit_thread = [[post.title, post.selftext]]
+    comment_list = []
+    main_count = -1  # Start main comment count at 0
+    for comment_id in sorted_ids:
+        if comment_id.body != '[removed]':
+            main_count += 1
+            sub_count = 0
+            comment_list.append([main_count, sub_count, comment_id.score, comment_id.id, ''])
+            if comment_id.replies:
+                sub_comments = create_subcomment_list(comment_id.replies, main_count)
+                comment_list.extend(sub_comments)
+    reddit_thread.extend(comment_list)
+    return reddit_thread
+
+
+def create_subcomment_list(replies):
+    sub_comments = []
+    for comment in replies:
+        if comment.body != '[removed]':
+            sub_comments.append([comment.score, comment.id, ''])
+            if comment.replies:
+                sub_sub_comments = create_subcomment_list(comment.replies, main_count)
+                sub_comments.extend(sub_sub_comments)
+    return sub_comments
+
+
+def get_replies(selection):
+    for selected_comment in selection.replies:
+        if selected_comment.body != '[removed]' and selected_comment.author != 'AutoModerator':
+            print(str(selected_comment.score) + ', ' + selected_comment.body[0:20] + ' ... ')
+            if selected_comment.replies:
+                get_replies(selected_comment)
+
+
+def create_dataset(post_url):
+    print('Loading data from URL')
+    post = REDDIT.submission(url=post_url)
+    post.comment_sort = "top"
+    post.comments.replace_more(limit=None)
+    # print('Loading {} commments past \'MoreComments\''.format(post.num_comments))
+    pprint.pprint(vars(post))
+    print(len(post.comments.list()))
+    comment_list = []
+    comment_count = 0
+    for selected_comment in post.comments:
+        if selected_comment.body != '[removed]' and selected_comment.author != 'AutoModerator':
+            print(str(selected_comment.score) + ', ' + selected_comment.body[0:20] + ' ... ')
+            if selected_comment.replies:
+                get_replies(selected_comment)
+                input('wait')
+            print('-----------------------------------')
+            comment_list.extend(selected_comment.replies)
+    input()
+    reddit_thread = create_comment_list(post)
+    for item in reddit_thread[1:30]:
+        print(item)
+
+
+create_dataset(askhistorians[1])
+# create_dataset(science[3])
